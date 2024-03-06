87a8d4b832b20e068fae15888666d95f5deba4de
mimi-kapadia12 <mansikapadia1999@gmail.com>
2024-03-06 22:47:31 +0530

    Fixed issue

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function Card({ title, value }) {
   return (
-    <div className="card text-center m-1">
+    <div className="card text-center m-1 weather-card">
       <div className="card-body">
         <p>{title}</p>
         <p>{value}</p>
