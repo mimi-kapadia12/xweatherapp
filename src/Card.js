2fe5d6608f8ddf48582d188008c400a7b176edfe
mimi-kapadia12 <mansikapadia1999@gmail.com>
2024-03-06 23:19:16 +0530

    changes

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ function Card({ title, value }) {
   return (
     <div className="card text-center m-1 weather-card">
       <div className="card-body">
-        <p>{title}</p>
+        <h5>{title}</h5>
         <p>{value}</p>
       </div>
     </div>
